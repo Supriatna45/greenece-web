e5855040476a6a05a389adb182a45a902dad4632
Supriatna <81977320+Supriatna45@users.noreply.github.com>
2021-12-14 22:26:56 +0700

    Update nama page

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title> Latihan Membuat Web</title>
+	<title>Login</title>
 <!-- 	<link rel="stylesheet" type="text/css" href="style11.css">
  -->	
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
