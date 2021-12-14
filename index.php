97292792892c204f8307e63f6c36ca167f2fe9e8
Supriatna <81977320+Supriatna45@users.noreply.github.com>
2021-12-14 22:59:35 +0700

    icon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Greenece</title>
+	<link href="assets/img/4.png" rel="icon">
 	<link rel="stylesheet" type="text/css" href="style11.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 </head>
--- success.php
+++ success.php
@@ -18,6 +18,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Succes Menambah</title>
+  <link href="assets/img/4.png" rel="icon">
 	<link rel="stylesheet" type="text/css" href="style11.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
