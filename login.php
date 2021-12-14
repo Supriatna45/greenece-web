cdde7ae5f1c0218cec7c488aec8607e8b2c0569b
Supriatna <81977320+Supriatna45@users.noreply.github.com>
2021-12-14 12:14:52 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,8 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<title> Latihan Membuat Web</title>
-	<link rel="stylesheet" type="text/css" href="style11.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<!-- 	<link rel="stylesheet" type="text/css" href="style11.css">
+ -->	
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
@@ -56,20 +57,14 @@
         ?>
         <form action = "<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" style="margin-top: -50px;">
             <br>
-            <div class="container">
                 <label for="usernamel" style="color: white;">Username</label>
                 <input required type="text" class="form-control" id="usernamel" name="usernamel">
-            </div>
             <br>
-            <div class="container">
                 <label for="passwordl" style="color: white;">Password</label>
                 <input required type="password" require class="form-control" id="passwordl" name="passwordl">
-            </div>
             <br>
             <br>
-            <div class = "container ">
             <button style="background-color:#37517e; border-color: #37517e;" type="submit" name="submit" class="btn btn-primary btn-md">Submit</button>
-            </div>
         </form>
 </body>
 </html>
